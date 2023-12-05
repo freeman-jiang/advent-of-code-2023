413136864098eb0caa88652a796644e575177482
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-12-05 01:06:21 -0500

    finish day 2

---
+++ day2/d2p2.py
@@ -0,0 +1,38 @@
+
+def initial_bag():
+    return {
+        'red': 12,
+        'blue': 14,
+        'green': 13
+    }
+
+
+def parse_line(l: str):
+    game_id, games = l.split(":")
+    game_id = int(game_id.split(" ")[1])
+
+    games = games.strip()
+    games = games.split(";")
+
+    max_bag = {x: 0 for x in initial_bag()}
+
+    for g in games:
+
+        rounds = g.split(",")
+        for r in rounds:
+            r = r.strip()
+            num, color = r.split(" ")
+            num = int(num)
+            max_bag[color] = max(num, max_bag[color])
+
+    return max_bag["red"] * max_bag["blue"] * max_bag["green"]
+
+
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+    res = 0
+
+    for line in lines:
+        res += parse_line(line)
+
+    print(res)
